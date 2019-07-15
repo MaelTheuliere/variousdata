532e601d8ca63581a5c89e3c736523f69728da9a
Maël Theulière <mael.theuliere@gmail.com>
2019-07-15 16:01:33 +0200

    - update

--- data-raw/data-raw-dvf.R
+++ data-raw/data-raw-dvf.R
@@ -6,7 +6,8 @@ options(scipen = 999)
 #source : https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/
   
 dvf_r52<-vroom(file = c("extdata/dvf/dvf_2014.csv","extdata/dvf/dvf_2017.csv")) %>% 
-  passer_au_cog_a_jour(code_commune = code_commune,garder_info_supra = F,aggrege = F) %>%
-  filter(str_sub(DEPCOM,1,2) %in% c("44","49","53","72","85"))
+  passer_au_cog_a_jour(code_commune = code_commune,garder_info_supra = T,aggrege = F) %>%
+  filter(str_sub(DEPCOM,1,2) %in% c("44","49","53","72","85")) %>% 
+  select(-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI)
 
 use_data(dvf_r52,overwrite = T)
